242a9b6fdd852f0da706e23b304c25beb31b5241
Daniel Enggaard <daen@itu.dk>
2023-02-27 13:43:43 +0100

    Added node clustering with alignment

--- src/graph_world/self_supervised_learning/pretext_tasks.py
+++ src/graph_world/self_supervised_learning/pretext_tasks.py
@@ -9,6 +9,7 @@ from dataclasses import dataclass
 from .__types import *
 from torch.nn import Module
 from torch import Tensor
+from sklearn.cluster import KMeans
 
 class BasicPretextTask(ABC):
     def __init__(self, data : InputGraph, encoder : Module, train_mask : Tensor, **kwargs): # **kwargs is needed
@@ -145,4 +146,54 @@ class AutoEncoding(BasicPretextTask):
 # ==================================================== #
 # ============= Auxiliary property-based ============= # 
 # ==================================================== #
+@gin.configurable
+class NodeClusteringWithAlignment(BasicPretextTask):
+    def __init__(self, n_clusters : int, random_state : int = 3364, **kwargs):
+        super().__init__(**kwargs)
 
+        # Step 0: Setup
+        train_mask = self.data.train_mask
+        X, y = self.data.x, self.data.y
+        num_classes = y.unique().shape[0]
+        feat_dim = X.shape[1]
+        centroids_labeled = torch.zeros((num_classes, feat_dim))
+
+        # Step 1: Compute centroids in each cluster by the mean in each class
+        for cn in range(num_classes):
+            lf = X[train_mask]
+            ll = y[train_mask]
+            centroids_labeled[cn] = lf[ll == cn].mean(axis=0)
+
+        # Step 2: Set cluster labels for each node
+        cluster_labels = torch.ones(y.shape, dtype=torch.int64) * -1
+        cluster_labels[train_mask] = y[train_mask]
+
+        # Step 3: Train KMeans on all points
+        kmeans = KMeans(n_clusters=n_clusters, random_state=random_state).fit(X)
+
+        # Step 4: Perform alignment mechanism
+        # See https://arxiv.org/pdf/1902.11038.pdf
+        # 1) Compute its centroids
+        # 2) Find cluster closest to the centroid computed in step 1
+        # 3) Assign all unlabeled nodes to that closest cluster.
+        for cn in range(n_clusters):
+            # v_l - Note this both includes the labeled and unlabeled according to the train_mask.
+            #       But the original authors implemented it like this.
+            centroids_unlabeled = X[kmeans.labels_ == cn].mean(axis=0)
+
+            # Equation 5
+            label_for_cluster = np.linalg.norm(centroids_labeled - centroids_unlabeled, axis=1).argmin()
+            for node in np.where(kmeans.labels_ == cn)[0]:
+                if not train_mask[node]:
+                    cluster_labels[node] = label_for_cluster
+                
+        self.pseudo_labels = cluster_labels
+        self.decoder = Linear(self.encoder.out_channels, num_classes)
+        self.loss = torch.nn.CrossEntropyLoss()
+
+
+
+    # Directly reconstruct input features from embedding
+    def make_loss(self, embeddings : Tensor) -> float:
+        y_hat = self.decoder(embeddings)
+        return self.loss(input=y_hat, target=self.pseudo_labels)
