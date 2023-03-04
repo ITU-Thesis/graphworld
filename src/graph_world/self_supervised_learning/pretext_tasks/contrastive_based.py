354c3038d031c65ce022744bd38d6681a68fece2
beke <beke@itu.dk>
2023-03-04 15:11:58 +0100

    implemented GCA

--- src/graph_world/self_supervised_learning/pretext_tasks/contrastive_based.py
+++ src/graph_world/self_supervised_learning/pretext_tasks/contrastive_based.py
@@ -9,7 +9,7 @@ from torch import Tensor
 import torch_geometric.nn.models.autoencoder as pyg_autoencoder
 from .basic_pretext_task import BasicPretextTask
 from ...models.basic_gnn import SuperGAT
-from torch_geometric.utils import negative_sampling, dropout_adj
+from torch_geometric.utils import negative_sampling, dropout_adj, degree
 from ..layers import NeuralTensorLayer
 from typing import Tuple
 
@@ -84,5 +84,58 @@ class GRACE(BasicPretextTask):
         l1 = self.compute_InfoNCE_loss(h1, h2)
         l2 = self.compute_InfoNCE_loss(h2, h1)
         return ((l1 + l2) * 0.5).mean()
+    
 
+# Based on https://github.com/CRIPAC-DIG/GCA
+# We only use node degree as centrality
+# - Can also use pagerank or eigenvector centrality
+@gin.configurable
+class GCA(GRACE):
+    def __init__(self, 
+                 tau : float = 0.5, 
+                 edge_mask_ratio1 : float = 0.2,
+                 edge_mask_ratio2 : float = 0.2,
+                 feature_mask_ratio1 : float = 0.2, 
+                 feature_mask_ratio2 : float = 0.2,
+                 **kwargs):
+        super().__init__(tau, edge_mask_ratio1, edge_mask_ratio2, 
+                         feature_mask_ratio1, feature_mask_ratio2, **kwargs)
+        
+        node_degrees = degree(self.data.edge_index[1])
+
+        # Edge masking weights
+        deg_col = node_degrees[self.data.edge_index[1]].to(torch.float32)
+        s_col = torch.log(deg_col)
+        self.edge_weights = (s_col.max() - s_col) / (s_col.max() - s_col.mean())
+
+
+        # Feature masking weights (for dense continous features)
+        # - Should be modified if features are discrete and sparse
+        x = self.data.x.abs()
+        w = x.t() @ node_degrees
+        w = w.log()
+        self.feature_weights = (w.max() - w) / (w.max() - w.mean())
+
+    # Drops edges based on edge_weights and mask_ratio
+    def drop_edges(self, e_mask_ratio : float, threshold : float = 0.7) -> Tensor:
+        edge_weights = self.edge_weights / self.edge_weights.mean() * e_mask_ratio
+        edge_weights = edge_weights.where(edge_weights < threshold, torch.ones_like(edge_weights) * threshold)
+        mask = torch.bernoulli(1. - edge_weights).to(torch.bool)
+        return self.data.edge_index[:, mask]
+    
+    # Drop features based on feature_weights and mask_ratio
+    def drop_features(self, f_mask_ratio: float, threshold:float = 0.7) -> Tensor:
+        w = self.feature_weights / self.feature_weights.mean() * f_mask_ratio
+        w = w.where(w < threshold, torch.ones_like(w) * threshold)
+        mask = torch.bernoulli(w).to(torch.bool)
+        features = self.data.x.clone()
+        features[:, mask] = 0
+        return features
+
+    # Override augmentation to take weights into account
+    def generate_view(self, f_mask_ratio : float, e_mask_ratio : float) -> Tuple[Tensor, Tensor]:
+        # Use threshold=0.7 similar to authors of method
+        edge_index = self.drop_edges(e_mask_ratio=e_mask_ratio, threshold=0.7) 
+        features = self.drop_features(f_mask_ratio=f_mask_ratio, threshold=0.7)
+        return features, edge_index
 
