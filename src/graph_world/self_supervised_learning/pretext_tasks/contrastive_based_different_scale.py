6d814b4a089f27cd9b217dc51f489cf089de4ada
Daniel Enggaard <daen@itu.dk>
2023-03-06 09:23:55 +0100

    Pushing deep graph infomax

--- src/graph_world/self_supervised_learning/pretext_tasks/contrastive_based_different_scale.py
+++ src/graph_world/self_supervised_learning/pretext_tasks/contrastive_based_different_scale.py
@@ -1,2 +1,28 @@
+from torch_geometric.nn.models.deep_graph_infomax import DeepGraphInfomax
+from torch import Tensor
+import torch.nn
+import gin
+from .basic_pretext_task import BasicPretextTask
 
+
+@gin.configurable
+class DeepGraphInfomax(BasicPretextTask):
+    '''
+    Deep Graph Infomax proposed in Velickovic, Petar, et al. "Deep graph infomax." ICLR (Poster) 2.3 (2019): 4.
+    '''
+    def __init__(self, **kwargs):
+
+        def summary_fn(h): return h.mean(dim=0)
+        def corruption(h): return h[torch.randperm(h.size()[0]), :]
+
+        self.dgi = DeepGraphInfomax(
+            hidden_channell=self.encoder.out_channel,
+            encoder=self.encoder,
+            summary=summary_fn,
+            corruption=corruption
+        )
+
+    def make_loss(self, embeddings: Tensor):
+        pos_z, neg_z, summary = self.dgi(self.data)
+        return self.dgi.loss(pos_z=pos_z, neg_z=neg_z, summary=summary)
 
