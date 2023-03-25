4a4c3bf544e048a2beaf06b6f0292c6715e253ba
beke <beke@itu.dk>
2023-03-25 19:25:23 +0100

    tested scripts for marginal graph properties, evaluated learning curves and adjusted hyperparams and preprocessed experiments for 30.0000 graphs

--- evaluation/evaluation_utils.py
+++ evaluation/evaluation_utils.py
@@ -1,15 +1,21 @@
 import json
 import pandas as pd
 
-def read_processed_shards(PROCESSED_DIR):
+def read_processed_shards(PROCESSED_DIR, shard=None):
     with open(f'{PROCESSED_DIR}/summary.json', 'r') as f:
         summary = json.load(f)
 
     dfs = []
-    for shard_idx in range(summary['N_RUNS']):
-        filename = f'{shard_idx+1}.ndjson'
+    file_names = []
+    if shard == None:
+        for shard_idx in range(summary['N_RUNS']):
+            filename = f'{shard_idx+1}.ndjson'
+            file_names.append(filename)
+    else:
+        file_names.append(f'{shard}.ndjson')
 
-        with open(f'{PROCESSED_DIR}/shards/{filename}', 'r') as f:
+    for f in file_names:
+        with open(f'{PROCESSED_DIR}/shards/{f}', 'r') as f:
             lines = f.readlines()
             records = map(json.loads, lines)
             dfs.append(pd.DataFrame.from_records(records))
