0d22ae4e50f0f69ea0f04cb1ccdaf77e8d3a1cc2
beke <beke@itu.dk>
2023-03-27 14:32:20 +0200

    evaluation utils

--- evaluation/evaluation_utils.py
+++ evaluation/evaluation_utils.py
@@ -15,12 +15,47 @@ def read_processed_shards(PROCESSED_DIR, shard=None):
         file_names.append(f'{shard}.ndjson')
 
     for f in file_names:
+        print(f)
         with open(f'{PROCESSED_DIR}/shards/{f}', 'r') as f:
             lines = f.readlines()
             records = map(json.loads, lines)
             dfs.append(pd.DataFrame.from_records(records))
 
     # Construct df
+    print("concatenating")
     results_df = pd.concat(dfs)
     del dfs
     return results_df
+
+
+
+def get_best_configuration_per_model(df, TEST_METRIC):
+    best_configurations = {}
+
+    # Common hparams
+    COMMON_PARAMS = ['encoder_in_channels', 'encoder_hidden_channels', 'encoder_num_layers', 'encoder_dropout']
+
+    # GAT hparams
+    GAT_PARAMS =['encoder_heads']
+
+    # JL hparams
+    JL_PARAMS = COMMON_PARAMS + ['train_pretext_weight']
+
+    # URL & PF hparams
+    TWO_STAGE_PARAMS = COMMON_PARAMS + ['train_pretext_epochs', 'train_pretext_lr']
+
+    models = [col.removesuffix(f'_{TEST_METRIC}') for col in df.columns if TEST_METRIC in col]
+    for model in models:
+        if "JL" in model:
+            params = [f'{model}_{p}' for p in JL_PARAMS]
+        else:
+            params = [f'{model}_{p}' for p in TWO_STAGE_PARAMS]
+        if "GAT" in model:
+            params += [f'{model}_{p}' for p in GAT_PARAMS]
+        test_metric = f'{model}_{TEST_METRIC}'
+        df_m = df[params + [test_metric]]
+        groups = df_m.groupby(params)
+        means = groups.mean()
+        best_configuration = means[test_metric].nlargest(1).reset_index()
+        best_configuration[model] = best_configuration
+    return best_configuration
