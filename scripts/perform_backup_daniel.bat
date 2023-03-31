825887a398b8ae7b4cb116bfee1711ebbf9c8022
danielenggaard1 <daniel@eos.dk>
2023-03-31 12:03:57 +0200

    Updated backup script

--- scripts/perform_backup_daniel.bat
+++ scripts/perform_backup_daniel.bat
@@ -1,2 +1,14 @@
-ren "C:\Users\danie\OneDrive\Skrivebord\backup" "C:\Users\danie\OneDrive\Skrivebord\backup_old"
-scp -i C:\Users\danie\.ssh\daniel_hpc -r daen@hpc.itu.dk:/home/data_shares/scara/graphworld/results/ C:\Users\danie\OneDrive\Skrivebord\bakcup
+@echo off
+
+echo Renaming directory...
+rename "C:\Users\danie\OneDrive\Skrivebord\backup" "backup_old"
+echo Directory renamed.
+
+
+echo Creating new directory...
+mkdir C:\Users\danie\OneDrive\Skrivebord\backup
+echo New directory created.
+
+echo Transferring files...
+scp -i C:\Users\danie\.ssh\daniel_hpc -r daen@hpc.itu.dk:/home/data_shares/scara/graphworld/results/ C:\Users\danie\OneDrive\Skrivebord\backup
+echo Files transferred.
