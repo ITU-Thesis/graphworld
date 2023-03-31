4d58f3ee81a7f242c07b515e3c6564f4cd84ffa7
danielenggaard1 <daniel@eos.dk>
2023-03-31 11:13:54 +0200

    Pushing backup script and experimentation.md

---
+++ scripts/perform_backup_daniel.bat
@@ -0,0 +1,2 @@
+ren "C:\Users\danie\OneDrive\Skrivebord\backup" "C:\Users\danie\OneDrive\Skrivebord\backup_old"
+scp -i C:\Users\danie\.ssh\daniel_hpc -r daen@hpc.itu.dk:/home/data_shares/scara/graphworld/results/ C:\Users\danie\OneDrive\Skrivebord\bakcup
